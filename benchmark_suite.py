1f399bd4a4fe328dc8c0825faf674dc8ed5b3071
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2019-09-02 11:02:57 +0200

    Implemented loading and saving of benchmark results

--- benchmark_suite.py
+++ benchmark_suite.py
@@ -2,8 +2,10 @@ import project_path
 from dataset import Dataset
 from sklearn.metrics import accuracy_score
 import glob
-from os.path import join
+from os.path import join, exists, isfile
 from timeout import call_with_timeout
+import json
+from collections import defaultdict
 
 class BenchmarkResults:
     """
@@ -42,28 +44,49 @@ class BenchmarkSuite:
     def get_XY_files(self, path):
         return [(file, '{}_Y.npy'.format(file.split('_X.')[0])) for file in glob.glob(join(path, '*.pickle'))]
 
-    def benchmark(self, classifiers):
+    def benchmark(self, classifiers, file='benchmark.json'):
+        prev_results = self.read_results(file)
         table = []
         step = 0
         num_steps = len(self.datasets) * len(classifiers)
         for ds in self.datasets:
+            prev_dataset_results = prev_results[ds.name] if ds.name in prev_results else {}
             row = []
             for classifier in classifiers:
-                Y_train = ds.get_labels_as_unique() if classifier.needs_unique_labels else ds.Y_train
-                classifier, success = call_with_timeout(classifier, 'fit', ds.X_train, Y_train, timeout=self.timeout)
-                if success:
-                    pred = classifier.predict(ds.X_train)
-                    if len(pred[pred == None]) != 0:
-                        stats = 'failed to fit'
-                    else:
-                        acc = accuracy_score(pred, Y_train)
-                        stats = classifier.get_stats()
-                        stats['accuracy'] = acc
+                if classifier.name in prev_dataset_results:
+                    stats = prev_dataset_results[classifier.name]
                 else:
-                    stats = 'timeout'
+                    Y_train = ds.get_labels_as_unique() if classifier.needs_unique_labels else ds.Y_train
+                    classifier, success = call_with_timeout(classifier, 'fit', ds.X_train, Y_train, timeout=self.timeout)
+                    if success:
+                        pred = classifier.predict(ds.X_train)
+                        if len(pred[pred == None]) != 0:
+                            stats = 'failed to fit'
+                        else:
+                            acc = accuracy_score(pred, Y_train)
+                            stats = classifier.get_stats()
+                            stats['accuracy'] = acc
+                    else:
+                        stats = 'timeout'
                 row.append(stats)
                 step += 1
-                print('Finished {}/{}.'.format(step, num_steps))
+                msg = 'Loaded' if classifier is not None and classifier.name in prev_dataset_results else 'Tested'
+                print('{} {}/{}.'.format(msg, step, num_steps))
             table.append(row)
         print('Done.')
-        return BenchmarkResults([ds.name for ds in self.datasets], [c.name for c in classifiers], table)
+        results = BenchmarkResults([ds.name for ds in self.datasets], [c.name for c in classifiers], table)
+        self.save_results(results, file)
+        return results
+
+    def save_results(self, results, file):
+        json_obj = defaultdict(dict)
+        for i in range(len(results.row_names)):
+            for j in range(len(results.column_names)):
+                json_obj[results.row_names[i]][results.column_names[j]] = results.table[i][j]
+        with open(file, 'w+') as outfile:
+            json.dump(json_obj, outfile, indent=4)
+
+    def read_results(self, file):
+        if not exists(file) or not isfile(file): return BenchmarkResults([], [], {})
+        with open(file, 'r') as infile:
+            return json.load(infile)
