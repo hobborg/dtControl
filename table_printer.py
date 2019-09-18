809ea6b426a4a960eca8c3d780aba4d3c2801428
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2019-09-18 12:27:38 +0200

    Added non-inline option for table printer

--- table_printer.py
+++ table_printer.py
@@ -1,23 +1,36 @@
 from IPython.display import HTML, display
+import webbrowser
+import os
 
-def print_table(benchmark_results):
-    output = '<html><body><table><tr><td></td>'
+def print_table(benchmark_results, inline=True):
+    head = '<head><link rel="stylesheet" ' \
+           'href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" ' \
+           'integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" ' \
+           'crossorigin="anonymous"></head>'
+
+    table_class = '' if inline else 'table table-striped table-bordered table-hover'
+    body = '<body><table class="{}"><thead><tr><td></td>'.format(table_class)
     for c in benchmark_results.column_names:
-        output += '<th scope="col" style="text-align:center">{}</th>'.format(c)
-    output += '</tr>'
+        body += '<th scope="col" style="text-align:center">{}</th>'.format(c)
+    body += '</tr></thead><tbody>'
 
     for i in range(len(benchmark_results.row_names)):
-        output += '<tr>'
-        output += '<th scope="row" style="text-align:center">{}</th>'.format(benchmark_results.row_names[i])
+        body += '<tr>'
+        body += '<th scope="row" style="text-align:center">{}</th>'.format(benchmark_results.row_names[i])
         for column in benchmark_results.table[i]:
-            output += '<td style="text-align:center">'
+            body += '<td style="text-align:center">'
             if isinstance(column, str):
-                output += '<span style="color:red">{}</span>'.format(column)
+                body += '<span style="color:red">{}</span>'.format(column)
             else:
                 for k, v in column.items():
-                    output += '{}: {}<br>'.format(k, v)
-            output += '</td>'
-        output += '</tr>'
-    output += '</table></body></html>'
+                    body += '{}: {}<br>'.format(k, v)
+            body += '</td>'
+        body += '</tr>'
+    body += '</tbody></table></body>'
 
-    display(HTML(output))
+    if inline:
+        display(HTML(f"<html>{body}</html>"))
+    else:
+        with open('tmp/benchmark.html', 'w+') as out:
+            out.write(f"<html>{head}{body}</html>")
+        webbrowser.open(f"file://{os.path.abspath('tmp/benchmark.html')}")
