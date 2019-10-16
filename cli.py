41a7f14900842b4a0932ccc25f9fe32112efa304
Pranav Ashok <pranavashok@gmail.com>
2019-10-16 17:35:13 +0200

    cli wip

--- cli.py
+++ cli.py
@@ -1,10 +1,10 @@
-'''
+"""
 README
 
-dtcontrol --input (<input_files>... | <input_folder>)
-          --output (<output_files>... | <output_folder>)
-          [--method <method_names>...] [--timeout <seconds>]
-          [--print-statistics <file>] [--load-statistics <file>]
+Usage: dtcontrol [-h] [-v] [--input INPUT [INPUT ...]] [--output OUTPUT]
+                 [--method METHOD [METHOD ...]]
+                 [--determinize DETSTRATEGY [DETSTRATEGY ...]]
+                 [--timeout TIMEOUT] [--benchmark-file FILENAME] [--rerun]
 
                 --input,-i  (<input_files>... | <input_folder>)
                         The input switch takes in one or more space separated file names
@@ -27,14 +27,22 @@ dtcontrol --input (<input_files>... | <input_folder>)
                             oc1         - oblique decision tree algorithm with randomized splits
                             all         - runs all of the above methods
                         If absent, defaults to --methods all
-                --print-statistics,-p <filename>
+                --determinize,-d <determinization_strategies>...
+                        In case of non-deterministic controllers, specify, if necessary the determinization
+                        strategy. Possible options are 'maxnorm', 'minnorm', 'maxfreq' and 'multimaxfreq'.
+                        If no determinization strategy is provided, then each set non-deterministic set
+                        of control inputs is treated uniquely.
+                --benchmark-file,-b <filename>
                         Saves statistics pertaining the construction of the decision trees and their
                         sizes into <filename>.json, and additionally allow to view it via <filename>.html
-                --load-statistics,-l <json_file>
-                        Loads the saved statistics and prevents re-running of already executed
-                        input-method combinations. Use along with --print-statistics to save
-                        any newly generated information to the same or different file.
+                --rerun, -r
+                        Rerun the experiment for all input-method combinations. Overrides the default
+                        behaviour of not running benchmarks for combinations which are already present
+                        in the benchmark file.
 
+                When multiple inputs with multiple methods and determinization strategies are passed,
+                dtControl tries to run every valid method-determinization strategy combination on each
+                input.
 
 
 Example:
@@ -54,13 +62,13 @@ dtcontrol --input dumps --output out --method cart --save_results res
 will read all valid controllers in dumps, run cart on them and print both dot
 and c files in out; and store stats in res
 
-'''
+"""
 
 import argparse
 import re
-
-from os.path import exists, isfile, splitext, dirname, isdir
+import sys
 from os import makedirs
+from os.path import exists, isfile, splitext
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
@@ -70,6 +78,7 @@ from classifiers.cart_custom_decision_tree import CartCustomDecisionTree
 from classifiers.linear_classifier_decision_tree import LinearClassifierDecisionTree
 from classifiers.max_every_node_decision_tree import MaxCartDecisionTree
 from classifiers.max_every_node_lc_decision_tree import MaxLCDecisionTree
+from classifiers.max_every_node_multi_decision_tree import MaxEveryNodeMultiDecisionTree
 from classifiers.oc1_wrapper import OC1Wrapper
 
 
@@ -98,37 +107,75 @@ def parse_timeout(timeout_string: str):
     unit_to_factor = {'s': 1, 'm': 60, 'h': 3600}
     if re.match(r'^[0-9]+[smh]$', timeout_string):
         factor = unit_to_factor[timeout_string[-1]]
-        timeout = int(args.timeout[:-1])*factor
+        timeout = int(args.timeout[:-1]) * factor
     else:
         # In case s, m or h is missing; then interpret number as timeout in seconds
         try:
             timeout = int(timeout_string)
         except ValueError:
-            raise Exception("Invalid value passed as timeout.")
+            parser.error("Invalid value passed as timeout.")
     return timeout
 
 
-def get_classifiers(method_list):
+def get_classifiers(methods, det_strategies):
     """
     Creates classifier objects for each method
 
-    :param method_list: list of method strings
+    :param methods: list of method strings
+    :param det_strategies: list of determinization strategies
     :return: list of classifier objects
     """
-    method_map = {'cart': CartCustomDecisionTree(),
-                  'maxcart': MaxCartDecisionTree(),
-                  'minnorm': MaxCartDecisionTree(),
-                  'maxnorm': MaxCartDecisionTree(),
-                  'maxlin': MaxLCDecisionTree(LogisticRegression, solver='lbfgs', penalty='none'),
-                  'linsvm': LinearClassifierDecisionTree(LinearSVC, max_iter=5000),
-                  'logreg': LinearClassifierDecisionTree(LogisticRegression, solver='lbfgs', penalty='none'),
-                  'oc1': OC1Wrapper(num_restarts=20, num_jumps=5)
-                  }
-    if not method_list or "all" in method_list:
-        return method_map.values()
-    else:
-        return [method_map[method] for method in method_list]
+    method_map = {
+        'cart': {
+            'nondet': CartCustomDecisionTree(),
+            'maxnorm': MaxCartDecisionTree(),
+            'minnorm': MaxCartDecisionTree(),
+            'maxfreq': MaxCartDecisionTree(),
+            'multimaxfreq': MaxEveryNodeMultiDecisionTree(),
+        },
+        'linsvm': {
+            'nondet': LinearClassifierDecisionTree(LinearSVC, max_iter=5000),
+            'maxnorm': MaxLCDecisionTree(LinearSVC, max_iter=5000),
+            'minnorm': MaxLCDecisionTree(LinearSVC, max_iter=5000),
+            'maxfreq': MaxLCDecisionTree(LinearSVC, max_iter=5000),
+        },
+        'logreg': {
+            'nondet': LinearClassifierDecisionTree(LogisticRegression, solver='lbfgs', penalty='none'),
+            'maxnorm': MaxLCDecisionTree(LogisticRegression, solver='lbfgs', penalty='none'),
+            'minnorm': MaxLCDecisionTree(LogisticRegression, solver='lbfgs', penalty='none'),
+            'maxfreq': MaxLCDecisionTree(LogisticRegression, solver='lbfgs', penalty='none'),
+        },
+        'oc1': {
+            None: OC1Wrapper(num_restarts=20, num_jumps=5)
+        }
+    }
 
+    if not methods:
+        return [method[strat] for method in method_map for strat in det_strategies]
+
+    if 'all' in methods:
+        return [cls for strats in method_map.values() for cls in strats.values()]
+
+    for method in methods:
+        if not method in method_map.keys():
+            parser.print_help()
+            sys.exit("Invalid method provided")
+
+    if not det_strategies:
+        return [method_map[method]['nondet'] for method in methods]
+    else:
+        classifiers = []
+        for method in methods:
+            if 'all' in det_strategies:
+                classifiers.append(method_map[method].values())
+            else:
+                for strat in det_strategies:
+                    try:
+                        classifiers.append(method_map[method][strat])
+                    except KeyError:
+                        parser.print_help()
+                        sys.exit("Invalid determinization strategy provided")
+        return classifiers
 
 parser = argparse.ArgumentParser(prog="dtcontrol")
 
@@ -144,28 +191,28 @@ parser.add_argument("--output", "-o", type=str,
 
 parser.add_argument("--method", "-m", default=['all'], nargs="+",
                     help="The method switch takes in one or more space separated method names as "
-                         "arguments. Available methods for axis-parallel splits are: 'cart', 'maxcart', "
-                         "'maxnorm', 'minnorm'; for oblique splits: 'maxlin', 'linsvm', 'logreg', 'oc1'; "
-                         "and 'all' for running all possible methods. For description about each method, "
+                         "arguments. Available methods are: 'cart', 'linsvm', 'logreg', 'oc1'. Running "
+                         "with --method 'all' will run all possible methods. For description about each method, "
                          "refer manual. If this switch is omitted, defaults to 'all'")
+
+parser.add_argument("--determinize", "-d", nargs='+', metavar='DETSTRATEGY', default='nondet',
+                    help="In case of non-deterministic controllers, specify, if necessary the determinization "
+                         "strategy. Possible options are 'maxnorm', 'minnorm', 'maxfreq' and 'multimaxfreq'")
 
 parser.add_argument("--timeout", "-t", type=str,
                     help="Sets a timeout for each method. Can be specified in seconds, minutes "
                          "or hours (eg. 300s, 7m or 3h)")
 
-parser.add_argument("--print-statistics", "-p", metavar="FILENAME", type=str,
+parser.add_argument("--benchmark-file", "-b", metavar="FILENAME", type=str,
                     help="Saves statistics pertaining the construction of the decision trees and their "
                          "sizes into a JSON file, and additionally allow to view it via an HTML file.")
 
-parser.add_argument("--load-statistics", "-l", metavar="JSON_FILE", type=(lambda x: is_valid_file(parser, x)),
-                    help="Loads the saved statistics and prevents re-running of already executed "
-                         "input-method combinations. Use along with --print-statistics to save"
-                         " any newly generated information to the same or different file.")
+parser.add_argument("--rerun", "-r", action='store_true',
+                    help="Rerun the experiment for all input-method combinations. Overrides the default "
+                         "behaviour of not running benchmarks for combinations which are already present"
+                         "in the benchmark file.")
 
-
-#parser.print_help()
 args = parser.parse_args()
-print(args)
 
 kwargs = dict()
 
@@ -173,32 +220,27 @@ dataset = []
 if args.input:
     dataset = args.input
 else:
-    raise Exception("Invalid input files/folders")
+    parser.print_help()
+    sys.exit("Input files/folders missing")
 
-kwargs["timeout"] = 2*60*60
+kwargs["timeout"] = 2 * 60 * 60
 if args.timeout:
     kwargs["timeout"] = parse_timeout(args.timeout)
 
-classifiers = get_classifiers(args.method)
-
-if args.print_statistics:
-    filename, file_extension = splitext(args.print_statistics)
-    kwargs["save_file"] = filename
+if args.benchmark_file:
+    filename, file_extension = splitext(args.benchmark_file)
+    kwargs["benchmark_file"] = filename
 
 if args.output:
     try:
         makedirs(args.output, exist_ok=True)
         kwargs["output_folder"] = args.output
     except PermissionError:
-        raise Exception("Ensure permission exists to create output directory")
+        sys.exit("Ensure permission exists to create output directory")
 
-if args.load_statistics:
-    kwargs["load_file"] = args.load_statistics
-    _, ext = splitext(args.load_statistics)
-    if not isfile(args.load_statistics):
-        raise Exception("Ensure file passed via --load-statistics/-l is a valid file")
-    if ext != "json":
-        raise Exception("Ensure file passed via --load-statistics/-l is a .json file")
+kwargs["rerun"] = args.rerun
+
+classifiers = get_classifiers(args.method, args.determinize)
 
 suite = BenchmarkSuite(**kwargs)
 suite.add_datasets(dataset)
