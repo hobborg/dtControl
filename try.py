b70883032c52c75dc4552333404b928b75dcdddb
Pranav Ashok <pranavashok@gmail.com>
2019-10-16 17:36:34 +0200

    fixed try.py after merging

--- try.py
+++ try.py
@@ -2,7 +2,7 @@ from benchmark_suite import BenchmarkSuite
 from classifiers.cart_custom_decision_tree import CartCustomDecisionTree
 from classifiers.max_every_node_decision_tree import MaxCartDecisionTree
 
-suite = BenchmarkSuite(timeout=60 * 60 * 2, save_folder='saved_classifiers', save_file='benchmark_tmp')
+suite = BenchmarkSuite(timeout=60 * 60 * 2, save_folder='saved_classifiers', benchmark_file='benchmark_tmp')
 suite.add_datasets(['../XYdatasets', '../dumps'],
                    include=["traffic_1m"],
                    exclude=[
