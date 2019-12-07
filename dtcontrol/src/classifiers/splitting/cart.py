a127b1f68ec7d7a459aa19e2638c844f119ae692
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2019-12-07 18:41:52 +0100

    Integrated impurity measure into CART

--- dtcontrol/src/classifiers/splitting/cart.py
+++ dtcontrol/src/classifiers/splitting/cart.py
@@ -1,27 +1,33 @@
-from sklearn.tree import DecisionTreeClassifier
-
 from src.classifiers.splitting.split import Split
 from src.classifiers.splitting.splitting_strategy import SplittingStrategy
 
 class CartSplittingStrategy(SplittingStrategy):
     def find_split(self, x, y, impurity_measure):
-        dt = DecisionTreeClassifier(max_depth=1, criterion='entropy')  # TODO: implement myself and use impurity measure
-        dt.fit(x, y)
-        mask = x[:, dt.tree_.feature[0]] <= dt.tree_.threshold[0]
-        return mask, CartSplit(dt.tree_)
+        splits = {}
+        for feature in range(x.shape[1]):
+            values = sorted(set(x[:, feature]))
+            for i in range(len(values) - 1):
+                threshold = (values[i] + values[i + 1]) / 2
+                mask = x[:, feature] <= threshold
+                splits[(feature, threshold)] = impurity_measure.calculate_impurity(y, mask)
+
+        feature, threshold = min(splits.keys(), key=splits.get)
+        mask = x[:, feature] <= threshold
+        return mask, CartSplit(feature, threshold)
 
 class CartSplit(Split):
-    def __init__(self, tree):
-        self.tree = tree
+    def __init__(self, feature, threshold):
+        self.feature = feature
+        self.threshold = threshold
 
     def predict(self, features):
-        return features[:, self.tree.feature[0]][0] <= self.tree.threshold[0]
+        return features[:, self.feature][0] <= self.threshold
 
     def print_dot(self):
         return self.print_c()
 
     def print_c(self):
-        return f'x[{self.tree.feature[0]}] <= {round(self.tree.threshold[0], 6)}'
+        return f'x[{self.feature}] <= {round(self.threshold, 6)}'
 
     def print_vhdl(self):
-        return f'x{self.tree.feature[0]} <= {round(self.tree.threshold[0], 6)}'
+        return f'x{self.feature} <= {round(self.threshold, 6)}'
