d3693127535fcde4bfc40f09fc65c6ac3f1e4007
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2020-01-20 16:09:58 +0100

    Small bugfix

--- dtcontrol/decision_tree/impurity/max_minority.py
+++ dtcontrol/decision_tree/impurity/max_minority.py
@@ -1,10 +1,18 @@
+import sys
+
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.impurity_measure import ImpurityMeasure
 
 class MaxMinority(ImpurityMeasure):
     def calculate_impurity(self, dataset, y, split):
-        return max([self.calculate_minority(y[mask]) for mask in split.get_masks(dataset)])
+        minorities = []
+        for mask in split.get_masks(dataset):
+            subset = y[mask]
+            if len(subset) == 0:
+                return sys.maxsize
+            minorities.append(self.calculate_minority(subset))
+        return max(minorities)
 
     @staticmethod
     def calculate_minority(y):
