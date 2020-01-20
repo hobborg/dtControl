89e664afb465c0629fc18cbaa7348849e58c2615
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2020-01-20 17:33:42 +0100

    Updated CI with different golf path

--- test/test_categorical.py
+++ test/test_categorical.py
@@ -11,9 +11,10 @@ class TestCategorical(unittest.TestCase):
     def test_categorical(self):
         try:
             ds = SingleOutputDataset('golf.csv')
+            ds.load_if_necessary()
         except FileNotFoundError:
             ds = SingleOutputDataset('test/golf.csv')
-        ds.load_if_necessary()
+            ds.load_if_necessary()
         self.assertEqual([0, 1, 2, 3], ds.x_metadata['categorical'])
         self.assertEqual(["Outlook", "Temperature", "Humidity", "Windy"], ds.x_metadata['variables'])
         self.assertEqual({
