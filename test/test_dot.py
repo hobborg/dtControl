81679178710d9215e9703e17d5eedfc8e03505f8
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2020-02-16 13:12:36 +0100

    Fixed dot printing test

--- test/test_dot.py
+++ test/test_dot.py
@@ -1,7 +1,6 @@
 import unittest
 
 from dtcontrol.dataset.multi_output_dataset import MultiOutputDataset
-from dtcontrol.dataset.single_output_dataset import SingleOutputDataset
 from dtcontrol.decision_tree.decision_tree import DecisionTree
 from dtcontrol.decision_tree.determinization.non_determinizer import NonDeterminizer
 from dtcontrol.decision_tree.impurity.entropy import Entropy
@@ -14,7 +13,7 @@ class TestDot(unittest.TestCase):
             ds = MultiOutputDataset('golf_multi.csv')
             ds.load_if_necessary()
         except FileNotFoundError:
-            ds = SingleOutputDataset('test/golf_multi.csv')
+            ds = MultiOutputDataset('test/golf_multi.csv')
             ds.load_if_necessary()
 
         dt = DecisionTree(NonDeterminizer(), [CategoricalMultiSplittingStrategy(), AxisAlignedSplittingStrategy()],
