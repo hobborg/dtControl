6083ad60ac93553bc08e8ebf348e7fa637c3cf56
Pranav Ashok <pranav.ashok@tum.de>
2020-03-03 23:50:53 +0100

    1. Ability to use YAML based configuration file. Use the --config and --preset switches. 2. Automatic semantic versioning, use #minor or #major to bump the minor/major versions. Pushing to master without one of these two hashtags will bump the patch version.

---
+++ dtcontrol/gen-semver.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# Adapted from https://gitlab.com/threedotslabs/ci-scripts/blob/master/common/gen-semver and
+# based on post at https://threedots.tech/post/automatic-semantic-versioning-in-gitlab-ci/
+import os
+import re
+import sys
+import semver
+import subprocess
+
+
+def git(*args):
+    return subprocess.check_output(["git"] + list(args))
+
+
+def tag_repo(tag):
+    url = os.environ["CI_REPOSITORY_URL"]
+
+    # Transforms the repository URL to the SSH URL
+    # Example input: https://gitlab-ci-token:xxxxxxxxxxxxxxxxxxxx@gitlab.com/threedotslabs/ci-examples.git
+    # Example output: git@gitlab.com:threedotslabs/ci-examples.git
+    push_url = re.sub(r'.+@([^/]+)/', r'git@\1:', url)
+
+    git("remote", "set-url", "--push", "origin", push_url)
+    git("tag", tag)
+    git("push", "origin", tag)
+
+
+def bump(latest):
+    commit_msg = git("log", "--format=%B", "-n", "1", "HEAD").decode().strip()
+    if "#minor" in commit_msg:
+        return semver.bump_minor(latest)
+    if "#major" in commit_msg:
+        return semver.bump_major(latest)
+    # Then use bump_patch, bump_minor or bump_major
+    return semver.bump_patch(latest)
+
+
+def main():
+    version_file = open("version", 'w')
+    try:
+        latest = git("describe", "--tags").decode().strip()
+        print(f"Current version: {latest}")
+    except subprocess.CalledProcessError:
+        # No tags in the repository
+        version = "1.0.0"
+    else:
+        # Skip already tagged commits
+        if '-' not in latest:
+            version_file.write(latest)
+            version_file.close()
+            print("Skipping tag update as latest commit already tagged.")
+            return 0
+
+        version = bump(latest)
+    print(f"Bumped version: {version}")
+
+    tag_repo(version)
+    version_file.write(latest)
+    version_file.close()
+    print("Saved to file 'version'")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
