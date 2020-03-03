9c045312e15e917bdb8740e85b242b9b57f56454
Pranav Ashok <ashok@in.tum.de>
2020-03-03 19:46:55 +0100

    minor changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dtcontrol",
-    version="2.0.0.dev1",
+    version="2.0.0.dev2",
     description="A small tool which can convert automatically synthesised formally verified "
                 "controllers into concise decision trees.",
     long_description=long_description,
