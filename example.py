be80a5c9b680278861baa3b456f363c8957ec850
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2020-03-05 18:57:57 +0100

    Fixed AUROC for numerical data

--- example.py
+++ example.py
@@ -2,9 +2,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 
 from dtcontrol.benchmark_suite import BenchmarkSuite
-from dtcontrol.decision_tree.decision_tree import DecisionTree
-from dtcontrol.decision_tree.determinization.non_determinizer import NonDeterminizer
-from dtcontrol.decision_tree.impurity.entropy import Entropy
 from dtcontrol.decision_tree.splitting.axis_aligned import AxisAlignedSplittingStrategy
 from dtcontrol.decision_tree.splitting.categorical_multi import CategoricalMultiSplittingStrategy
 from dtcontrol.decision_tree.splitting.linear_classifier import LinearClassifierSplittingStrategy
@@ -13,14 +10,14 @@ from dtcontrol.decision_tree.splitting.oc1 import OC1SplittingStrategy
 suite = BenchmarkSuite(timeout=60 * 60 * 3,
                        save_folder='saved_classifiers',
                        benchmark_file='benchmark',
-                       rerun=True)
+                       rerun=False)
 
 suite.add_datasets(['examples', 'examples/prism'],
                    include=[
                        # "firewire_abst",
                        # "wlan0",
                        # "mer10"
-                       # "cartpole",
+                       "cartpole",
                        # "tworooms-noneuler-latest",
                        # "helicopter",
                        # "cruise-latest",
@@ -41,8 +38,10 @@ oc1 = OC1SplittingStrategy(num_restarts=10, num_jumps=5)
 logreg = LinearClassifierSplittingStrategy(LogisticRegression, solver='lbfgs', penalty='none')
 linsvc = LinearClassifierSplittingStrategy(LinearSVC, max_iter=5000)
 classifiers = [
-    DecisionTree(NonDeterminizer(), [aa, cat], Entropy(), 'CategoricalCART')
-    # DecisionTree(NonDeterminizer(), [aa], Entropy(), 'CART'),
+    # DecisionTree(NonDeterminizer(), [aa, cat], Entropy(), 'CategoricalCART')
+    # DecisionTree(NonDeterminizer(), [aa], Entropy(), 'CART-ent'),
+    # DecisionTree(NonDeterminizer(), [aa], AUROC(), 'CART-auc'),
+    # DecisionTree(NonDeterminizer(), [aa, logreg], AUROC(), 'CART-logreg-auc'),
     # DecisionTree(NonDeterminizer(), [aa, linsvc], Entropy(), 'linsvc'),
     # DecisionTree(NonDeterminizer(), [aa, logreg], Entropy(), 'logreg'),
     # DecisionTree(NonDeterminizer(), [aa, oc1], Entropy(), 'OC1'),
