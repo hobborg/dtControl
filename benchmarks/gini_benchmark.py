fa7a9b8991f8b1e415a3912b41d405c5074701fb
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2020-03-14 22:25:02 +0100

    Added benchmarks with gini index

--- benchmarks/gini_benchmark.py
+++ benchmarks/gini_benchmark.py
@@ -7,18 +7,14 @@ from dtcontrol.decision_tree.determinization.non_determinizer import NonDetermin
 from dtcontrol.decision_tree.impurity.entropy import Entropy
 from dtcontrol.decision_tree.impurity.gini_index import GiniIndex
 from dtcontrol.decision_tree.splitting.axis_aligned import AxisAlignedSplittingStrategy
-from dtcontrol.decision_tree.splitting.categorical_multi import CategoricalMultiSplittingStrategy
 from dtcontrol.decision_tree.splitting.linear_classifier import LinearClassifierSplittingStrategy
 
 suite = BenchmarkSuite(timeout=60 * 3,
-                       benchmark_file='benchmarks/impurity',
+                       benchmark_file='benchmarks/gini',
                        rerun=False)
 
 suite.add_datasets(['examples', 'examples/prism'],
                    include=[
-                       "firewire_abst",
-                       "wlan0",
-                       "mer10"
                        "cartpole",
                        "tworooms-noneuler-latest",
                        # "helicopter",
@@ -36,19 +32,13 @@ suite.add_datasets(['examples', 'examples/prism'],
 
 aa = AxisAlignedSplittingStrategy()
 logreg = LinearClassifierSplittingStrategy(LogisticRegression, solver='lbfgs', penalty='none')
-categorical = CategoricalMultiSplittingStrategy()
-grouping = CategoricalMultiSplittingStrategy(value_grouping=True)
 classifiers = [
     DecisionTree(NonDeterminizer(), [aa], Entropy(), 'CART-ent'),
     DecisionTree(NonDeterminizer(), [aa], GiniIndex(), 'CART-gini'),
     DecisionTree(NonDeterminizer(), [aa, logreg], Entropy(), 'logreg-ent'),
     DecisionTree(NonDeterminizer(), [aa, logreg], GiniIndex(), 'logreg-gini'),
     DecisionTree(MaxFreqDeterminizer(), [aa], Entropy(), 'MaxFreq-ent'),
-    DecisionTree(MaxFreqDeterminizer(), [aa], GiniIndex(), 'MaxFreq-gini'),
-    DecisionTree(NonDeterminizer(), [aa, categorical], Entropy(), 'Cat-ent'),
-    DecisionTree(NonDeterminizer(), [aa, categorical], GiniIndex(), 'Cat-gini'),
-    DecisionTree(NonDeterminizer(), [aa, grouping], Entropy(), 'Group-ent'),
-    DecisionTree(NonDeterminizer(), [aa, grouping], GiniIndex(), 'Group-gini'),
+    DecisionTree(MaxFreqDeterminizer(), [aa], GiniIndex(), 'MaxFreq-gini')
 ]
 suite.benchmark(classifiers)
 suite.display_html()
---
+++ benchmarks/gini_prism_benchmark.py
@@ -0,0 +1,31 @@
+from dtcontrol.benchmark_suite import BenchmarkSuite
+from dtcontrol.decision_tree.decision_tree import DecisionTree
+from dtcontrol.decision_tree.determinization.non_determinizer import NonDeterminizer
+from dtcontrol.decision_tree.impurity.entropy import Entropy
+from dtcontrol.decision_tree.impurity.gini_index import GiniIndex
+from dtcontrol.decision_tree.splitting.axis_aligned import AxisAlignedSplittingStrategy
+from dtcontrol.decision_tree.splitting.categorical_multi import CategoricalMultiSplittingStrategy
+from dtcontrol.decision_tree.splitting.categorical_single import CategoricalSingleSplittingStrategy
+
+suite = BenchmarkSuite(timeout=60,
+                       benchmark_file='benchmarks/gini_prism',
+                       rerun=False)
+
+suite.add_datasets(['examples/prism'])
+
+aa = AxisAlignedSplittingStrategy()
+categorical = CategoricalMultiSplittingStrategy()
+grouping = CategoricalMultiSplittingStrategy(value_grouping=True)
+single = CategoricalSingleSplittingStrategy()
+classifiers = [
+    DecisionTree(NonDeterminizer(), [aa, categorical], Entropy(), 'Cat-ent'),
+    DecisionTree(NonDeterminizer(), [aa, categorical], GiniIndex(), 'Cat-gini'),
+    DecisionTree(NonDeterminizer(), [aa, grouping], Entropy(), 'Group-ent'),
+    DecisionTree(NonDeterminizer(), [aa, grouping], GiniIndex(), 'Group-gini'),
+    DecisionTree(NonDeterminizer(), [aa, single], Entropy(), 'Single-ent'),
+    DecisionTree(NonDeterminizer(), [aa, single], GiniIndex(), 'Single-gini'),
+    DecisionTree(NonDeterminizer(), [aa, single, categorical], Entropy(), 'Both-ent'),
+    DecisionTree(NonDeterminizer(), [aa, single, categorical], GiniIndex(), 'Both-gini'),
+]
+suite.benchmark(classifiers)
+suite.display_html()
