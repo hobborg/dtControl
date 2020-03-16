02f1519416d9e78d1064fa0b1c60cef6912a4dbf
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2020-03-16 14:32:46 +0100

    Added benchmark for entropy ratio

---
+++ benchmarks/impurity/ratio/ratio_benchmark.py
@@ -0,0 +1,44 @@
+from sklearn.linear_model import LogisticRegression
+
+from dtcontrol.benchmark_suite import BenchmarkSuite
+from dtcontrol.decision_tree.decision_tree import DecisionTree
+from dtcontrol.decision_tree.determinization.max_freq_determinizer import MaxFreqDeterminizer
+from dtcontrol.decision_tree.determinization.non_determinizer import NonDeterminizer
+from dtcontrol.decision_tree.impurity.entropy import Entropy
+from dtcontrol.decision_tree.impurity.entropy_ratio import EntropyRatio
+from dtcontrol.decision_tree.splitting.axis_aligned import AxisAlignedSplittingStrategy
+from dtcontrol.decision_tree.splitting.linear_classifier import LinearClassifierSplittingStrategy
+
+suite = BenchmarkSuite(timeout=60 * 3,
+                       benchmark_file='ratio',
+                       rerun=False)
+
+suite.add_datasets('../../../examples',
+                   include=[
+                       "cartpole",
+                       "tworooms-noneuler-latest",
+                       # "helicopter",
+                       "cruise-latest",
+                       # "dcdc",
+                       "10rooms",
+                       # "truck_trailer",
+                       # "traffic_1m",
+                       # "traffic_10m",
+                       # "traffic_30m",
+                       # "vehicle",
+                       # "aircraft"
+                   ]
+                   )
+
+aa = AxisAlignedSplittingStrategy()
+logreg = LinearClassifierSplittingStrategy(LogisticRegression, solver='lbfgs', penalty='none')
+classifiers = [
+    DecisionTree(NonDeterminizer(), [aa], Entropy(), 'CART-ent'),
+    DecisionTree(NonDeterminizer(), [aa], EntropyRatio(), 'CART-ratio'),
+    DecisionTree(NonDeterminizer(), [aa, logreg], Entropy(), 'logreg-ent'),
+    DecisionTree(NonDeterminizer(), [aa, logreg], EntropyRatio(), 'logreg-ratio'),
+    DecisionTree(MaxFreqDeterminizer(), [aa], Entropy(), 'MaxFreq-ent'),
+    DecisionTree(MaxFreqDeterminizer(), [aa], EntropyRatio(), 'MaxFreq-ratio')
+]
+suite.benchmark(classifiers)
+suite.display_html()
