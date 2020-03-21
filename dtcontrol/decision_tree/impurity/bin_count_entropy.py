b5776d2baf8df143bb37913fe4abb83a9cdd9ef9
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2020-03-21 15:00:33 +0100

    update

--- dtcontrol/decision_tree/impurity/bin_count_entropy.py
+++ dtcontrol/decision_tree/impurity/bin_count_entropy.py
@@ -1,10 +1,17 @@
 import sys
 
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 
+# matplotlib.use('TkAgg')
+from scipy.interpolate import CubicSpline
+
+from dtcontrol.decision_tree.determinization.max_freq_determinizer import MaxFreqDeterminizer
 from dtcontrol.decision_tree.impurity.impurity_measure import ImpurityMeasure
 
 class AvgBinCount(ImpurityMeasure):
+
     def calculate_impurity(self, dataset, y, split):
         if len(split.get_masks(dataset)) == 1:
             return sys.maxsize
@@ -17,7 +24,21 @@ class AvgBinCount(ImpurityMeasure):
         return impurity
 
     @staticmethod
+    def scaling_function(x):
+        return -x * np.log2(x)
+        # return 1 - x**5
+        # return 1 - 2.0238 * np.power(x, 3) - 0.916667 * np.power(x, 2) - 0.20714 * x + 0.101
+        # return 1 - 2 * (1 - 1 / (x + 1))
+        # return 1/x
+        # return 1 - x * (x + .5)
+
+    @staticmethod
     def calculate_avg_bincount(y):
+        def plot(counts):
+            plt.figure()
+            plt.bar(range(1, len(counts) + 1), counts)
+            plt.show()
+
         flattened_labels = y.flatten()
         # remove -1 as we use it only as a filler
         flattened_labels = flattened_labels[flattened_labels != -1]
@@ -25,30 +46,11 @@ class AvgBinCount(ImpurityMeasure):
         if np.any(label_counts == len(y)):
             return 0
         label_counts = label_counts[label_counts != 0].astype('float')
-        label_counts /= np.sum(label_counts)  # TODO MJA: with this directly or entropy
-        # return 1 / np.mean(label_counts)
-        return sum(-prob * np.log2(prob) for prob in label_counts)
-
-        # label_counts *= label_counts + 0.5
-        # order = np.argsort(label_counts)
-        # total = len(label_counts) * (len(label_counts) + 1) / 2
-        # weighted = np.array([(i / total) * label_counts[order[i]] for i in range(len(label_counts))])
-        # return 1 / np.mean(weighted)
-
-        # return 1 / np.sum(label_counts[label_counts != 0] ** 2)
-
-        # return 1 / np.mean(label_counts ** 6)
-
-        # label_counts *= label_counts + 0.5
-        # label_counts *= label_counts + 0.5
-        # return 1 / np.mean(label_counts)
-
-    @staticmethod
-    def calculate_entropy(y):
-        num_labels = len(y)
-        unique = np.unique(y)
-        probabilities = [len(y[y == label]) / num_labels for label in unique]
-        return sum(-prob * np.log2(prob) for prob in probabilities)
+        label_counts = label_counts / len(y)
+        # probs = label_counts / len(flattened_labels)
+        scaled = AvgBinCount.scaling_function(label_counts)
+        # return sum(scaled)
+        return sum(scaled)
 
     def get_oc1_name(self):
         return None
--- dtcontrol/decision_tree/impurity/nondet_entropy_ratio.py
+++ dtcontrol/decision_tree/impurity/nondet_entropy_ratio.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from dtcontrol.decision_tree.impurity.impurity_measure import ImpurityMeasure
 
-class EntropyRatio(ImpurityMeasure):
+class NondetEntropyRatio(ImpurityMeasure):
     def calculate_impurity(self, dataset, y, split):
         if any(len(y[mask]) == 0 for mask in split.get_masks(dataset)) or \
                 len(split.get_masks(dataset)) == 1:
@@ -13,22 +13,28 @@ class EntropyRatio(ImpurityMeasure):
         split_entropy = self.calculate_split_entropy(dataset, y, split)
         split_info = self.calculate_split_info(dataset, y, split)
         return split_entropy / split_info
+        # return split_entropy
 
     @staticmethod
     def calculate_split_entropy(dataset, y, split):
         entropy = 0
         for mask in split.get_masks(dataset):
             subset = y[mask]
-            entropy += (len(subset) / len(y)) * EntropyRatio.calculate_entropy(subset)
+            entropy += (len(subset) / len(y)) * NondetEntropyRatio.calculate_entropy(subset)
         assert entropy >= 0
         return entropy
 
     @staticmethod
     def calculate_entropy(y):
-        num_labels = len(y)
-        unique = np.unique(y)
-        probabilities = [len(y[y == label]) / num_labels for label in unique]
-        return sum(-prob * np.log2(prob) for prob in probabilities)
+        flattened_labels = y.flatten()
+        # remove -1 as we use it only as a filler
+        flattened_labels = flattened_labels[flattened_labels != -1]
+        label_counts = np.bincount(flattened_labels)[1:]
+        if np.any(label_counts == len(y)):
+            return 0
+        label_counts = label_counts[label_counts != 0].astype('float')
+        label_counts = label_counts / len(y)
+        return sum(-p * np.log2(p) for p in label_counts)
 
     @staticmethod
     def calculate_split_info(dataset, y, split):
