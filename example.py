0e57a42d287000bb164b638a56081a932e7d1568
ga67vib <ga67vib@mytum.de>
2020-04-16 18:54:21 +0200

    BDDs working now

--- example.py
+++ example.py
@@ -11,6 +11,7 @@ from dtcontrol.decision_tree.splitting.axis_aligned import AxisAlignedSplittingS
 from dtcontrol.decision_tree.splitting.categorical_multi import CategoricalMultiSplittingStrategy
 from dtcontrol.decision_tree.splitting.linear_classifier import LinearClassifierSplittingStrategy
 from dtcontrol.decision_tree.splitting.oc1 import OC1SplittingStrategy
+from dtcontrol.bdd import BDD
 
 suite = BenchmarkSuite(timeout=60 * 60 * 3,
                        save_folder='saved_classifiers',
@@ -24,16 +25,16 @@ suite.add_datasets(['examples', 'examples/prism'],
                        # "mer10"
                        "cartpole",
                        # "tworooms-noneuler-latest",
-                       # "helicopter",
-                       # "cruise-latest",
+                        "helicopter",
+                        "cruise-latest",
                        # "dcdc",
-                       # "10rooms",
+                        "10rooms",
                        # "truck_trailer",
                        # "traffic_1m",
                        # "traffic_10m",
-                       # "traffic_30m",
-                       # "vehicle",
-                       # "aircraft"
+                        "traffic_30m",
+                        "vehicle",
+                        "aircraft"
                    ]
                    )
 
@@ -43,12 +44,16 @@ oc1 = OC1SplittingStrategy(num_restarts=10, num_jumps=5)
 logreg = LinearClassifierSplittingStrategy(LogisticRegression, solver='lbfgs', penalty='none',
                                            determinizer=MaxFreqDeterminizer())
 linsvc = LinearClassifierSplittingStrategy(LinearSVC, max_iter=5000, determinizer=MaxFreqDeterminizer())
+bdd_actOR = BDD(0)
+bdd_actUL = BDD(1)
 classifiers = [
-    DecisionTree([aa], Entropy(), 'CART'),
-    DecisionTree([aa], Entropy(), 'Min', label_pre_processor=NormPreProcessor(min)),
-    DecisionTree([aa], Entropy(), 'Rand', label_pre_processor=RandomPreProcessor()),
-    DecisionTree([aa], Entropy(MaxFreqDeterminizer()), 'MaxFreq', early_stopping=True),
-    DecisionTree([aa], Entropy(MaxFreqDeterminizer(pre_determinize=False)), 'MaxFreq-post', early_stopping=True),
+    #bdd_actOR,
+    bdd_actUL
+    #DecisionTree([aa], Entropy(), 'CART'),
+    #DecisionTree([aa], Entropy(), 'Min', label_pre_processor=NormPreProcessor(min)),
+    #DecisionTree([aa], Entropy(), 'Rand', label_pre_processor=RandomPreProcessor()),
+    #DecisionTree([aa], Entropy(MaxFreqDeterminizer()), 'MaxFreq', early_stopping=True),
+    #DecisionTree([aa], Entropy(MaxFreqDeterminizer(pre_determinize=False)), 'MaxFreq-post', early_stopping=True),
 ]
 suite.benchmark(classifiers)
 suite.display_html()
