80a8137f05f2d6fbf1ffb172123eaf546ee8f6fe
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2020-05-04 16:11:04 +0200

    Updated artifacts to only run the necessary benchmarks #nobump

--- web/files/qest20-artifact-subset.py
+++ web/files/qest20-artifact-subset.py
@@ -7,32 +7,37 @@ from dtcontrol.decision_tree.splitting.axis_aligned import AxisAlignedSplittingS
 from dtcontrol.decision_tree.splitting.categorical_multi import CategoricalMultiSplittingStrategy
 from dtcontrol.pre_processing.norm_pre_processor import NormPreProcessor
 
-suite = BenchmarkSuite(save_folder='saved_classifiers',
-                       benchmark_file='benchmark',
-                       rerun=False)
-
-suite.add_datasets(['dtcontrol-examples', 'dtcontrol-examples/prism'],
-                   include=[
-                       "firewire_abst",
-                       "ij.10",
-                       "leader4",
-                       "beb.3-4.LineSeized",
-                       "csma2_4_max",
-                       "eajs.2.100.5.ExpUtil",
-                       "wlan2",
-                       "zeroconf",
-                       "cartpole",
-                       "10rooms"
-                   ]
-                   )
-
 aa = AxisAlignedSplittingStrategy()
 cat = CategoricalMultiSplittingStrategy(value_grouping=True)
 bdd_actOR = BDD(0, label_pre_processor=NormPreProcessor(min))
 bdd_actUL = BDD(1, label_pre_processor=NormPreProcessor(min))
-classifiers = [
-    DecisionTree([aa], MultiLabelEntropy(), 'Multi-label', early_stopping=True),
+
+mdp_classifiers = [
     DecisionTree([aa, cat], Entropy(), 'AVG'),
     bdd_actUL
 ]
-suite.benchmark(classifiers)
+cps_classifiers = [
+    DecisionTree([aa], MultiLabelEntropy(), 'Multi-label', early_stopping=True),
+    bdd_actUL
+]
+
+suite = BenchmarkSuite(benchmark_file='benchmark')
+
+suite.add_datasets(['dtcontrol-examples', 'dtcontrol-examples/prism'], include=[
+    "firewire_abst",
+    "ij.10",
+    "leader4",
+    "beb.3-4.LineSeized",
+    "csma2_4_max",
+    "eajs.2.100.5.ExpUtil",
+    "wlan2",
+    "zeroconf",
+])
+suite.benchmark(mdp_classifiers)
+
+suite.add_datasets(['dtcontrol-examples'], include=[
+    "cartpole",
+    "10rooms",
+])
+suite.benchmark(cps_classifiers)
+suite.display_html()
--- web/files/qest20-artifact.py
+++ web/files/qest20-artifact.py
@@ -7,37 +7,42 @@ from dtcontrol.decision_tree.splitting.axis_aligned import AxisAlignedSplittingS
 from dtcontrol.decision_tree.splitting.categorical_multi import CategoricalMultiSplittingStrategy
 from dtcontrol.pre_processing.norm_pre_processor import NormPreProcessor
 
-suite = BenchmarkSuite(save_folder='saved_classifiers',
-                       benchmark_file='benchmark',
-                       rerun=False)
-
-suite.add_datasets(['dtcontrol-examples', 'dtcontrol-examples/prism'],
-                   include=[
-                       "firewire_abst",
-                       "ij.10",
-                       "leader4",
-                       "beb.3-4.LineSeized",
-                       "csma2_4_max",
-                       "eajs.2.100.5.ExpUtil",
-                       "wlan2",
-                       "zeroconf",
-                       "mer30",
-                       "exploding-blocksworld.5",
-                       "firewire.true.3.200.time_min",
-                       "cartpole",
-                       "10rooms",
-                       "helicopter"
-                   ]
-                   )
-
 aa = AxisAlignedSplittingStrategy()
 cat = CategoricalMultiSplittingStrategy(value_grouping=True)
 bdd_actOR = BDD(0, label_pre_processor=NormPreProcessor(min))
 bdd_actUL = BDD(1, label_pre_processor=NormPreProcessor(min))
-classifiers = [
-    DecisionTree([aa], MultiLabelEntropy(), 'Multi-label', early_stopping=True),
+
+mdp_classifiers = [
     DecisionTree([aa, cat], Entropy(), 'AVG'),
+    bdd_actUL,
+    bdd_actOR]
+cps_classifiers = [
+    DecisionTree([aa], MultiLabelEntropy(), 'Multi-label', early_stopping=True),
     bdd_actOR,
     bdd_actUL
 ]
-suite.benchmark(classifiers)
+
+suite = BenchmarkSuite(benchmark_file='benchmark')
+
+suite.add_datasets(['dtcontrol-examples', 'dtcontrol-examples/prism'], include=[
+    "firewire_abst",
+    "ij.10",
+    "leader4",
+    "beb.3-4.LineSeized",
+    "csma2_4_max",
+    "eajs.2.100.5.ExpUtil",
+    "wlan2",
+    "zeroconf",
+    "mer30",
+    "exploding-blocksworld.5",
+    "firewire.true.3.200.time_min",
+])
+suite.benchmark(mdp_classifiers)
+
+suite.add_datasets(['dtcontrol-examples'], include=[
+    "cartpole",
+    "10rooms",
+    "helicopter"
+])
+suite.benchmark(cps_classifiers)
+suite.display_html()
