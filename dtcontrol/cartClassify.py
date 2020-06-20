3794cad8b0f731e6b06983d9817c6ffe8fe5789e
Mayank <yadav.mayank2609@gmail.com>
2020-06-21 01:44:27 +0530

    Got classify working from tool correctly, buttons not disabled now all the time

--- dtcontrol/cartClassify.py
+++ dtcontrol/cartClassify.py
@@ -11,102 +11,11 @@ bound_x1 = 2
 world_dim_x = 10
 steps_beyond_done = None
 
-def getBounds():
-    return  [[lb_x0,ub_x0],[-bound_x1,bound_x1]]
-
-def classify(x0,x1):
-    dumstr = ""
-
-    x0 = float(x0)
-    x1 = float(x1)
-    
-    x0*=100
-    x0=math.floor(x0)
-    x0-=224
-    quo = math.floor(x0/8)
-    x0= 2.24+0.08*quo
-
-    x1*=10
-    x1=math.floor(x1)
-    x1/=10
-
-    # print('####################x0 is',x0,' and x1 is ',x1)
-
-    if (x1 <= -0.05):
-        dumstr="0"
-        if (x0 <= 3.64):
-            dumstr+="0"
-            if (x1 <= -0.35):
-                dumstr+="0"
-                if (x0 <= 3.56):
-                    dumstr+="0"
-                    return 4.0,dumstr
-
-                else :
-                    dumstr+="1"
-                    return 0.6,dumstr
-
-            else :
-                dumstr+="1"
-                if (x0 <= 2.36):
-                    dumstr+="0"
-                    return 3.9,dumstr
-                else :
-                    dumstr+="1"
-                    return 2.0,dumstr
-
-        else :
-            dumstr+="1"
-            if (x1 <= -1.85):
-                dumstr+="0"
-                return 0.3,dumstr
-
-            else :
-                dumstr+="1"
-                if (x1 <= -0.15):
-                    dumstr+="0"
-                    return 0.0,dumstr
-
-                else :
-                    dumstr+="1"
-                    return -0.8,dumstr
-
-    else :
-        dumstr+="1"
-        if (x0 <= 2.76):
-            dumstr+="0"
-            if (x1 <= 0.15):
-                dumstr+="0"
-                return 1.8,dumstr
-
-            else :
-                dumstr+="1"
-                if (x1 <= 0.25) :
-                    dumstr+="0"
-                    return 0.0,dumstr
-
-                else :
-                    dumstr+="1"
-                    return -0.6,dumstr
-
-        else :
-            dumstr+="1"
-            if (x1 <= 0.15) :
-                dumstr+="0"
-                if (x0 <= 2.84) :
-                    dumstr+="0"
-                    return 0.0,dumstr
-
-                else :
-                    dumstr+="1"
-                    return -3.0,dumstr
-
-            else :
-                dumstr+="1"
-                return -4.0,dumstr
+# def getBounds():
+#     return  [[lb_x0,ub_x0],[-bound_x1,bound_x1]]
 
 def f(t, x0, x1): 
-    return x1 
+    return x1*2
 
 def g(t, x0, x1,u):
     return (-omega * omega * (math.sin(x0)+u * math.cos(x0))- 2 * ga * x1)
@@ -143,15 +52,14 @@ def rungeKutta(x0, y0, z0, x, h, u):
     return y,z
 
 def step(x,u):
-    x0, x1= x
-    x0=float(x0)
-    x1=float(x1)
-    u=float(u)
+    # way of changing this would vary model to model
+    # x0, x1= x
+    x0=float(x[0])
+    x1=float(x[1])
+    u=float(u[0])
     
     costheta = math.cos(x0) 
     sintheta = math.sin(x0)
-
-    # u = classify(x0,x1)
 
     derut = rungeKutta(0,x0,x1,tau,0.01,u)
     x0 = derut[0]
@@ -176,6 +84,7 @@ def step(x,u):
     #     steps_beyond_done += 1
     #     reward = 0.0
 
-    newu,path = classify(x0,x1)
-
-    return x,newu,path,done
+    # newu,path = classify(x0,x1)
+    # newu is expected to be an array
+    # return x,newu,path,done
+    return x
