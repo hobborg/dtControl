3f9bb82b81b9f806a2bd11b0836957ce17cd65c6
Mayank <yadav.mayank2609@gmail.com>
2020-07-15 18:54:09 +0530

    Sympy works perfectly, no need of cartClassify now. Need to resolve discretisation issue

--- dtcontrol/visualise/fileToPy.py
+++ dtcontrol/visualise/fileToPy.py
@@ -30,21 +30,27 @@ for i in my_list:
                 foo = i.split("=")
                 tmp = sp.sympify(foo[1].strip())
                 tmp = tmp.subs(variable_subs)
-                lambda_list.append((foo[0].strip(), tmp))
+                lam_1 = sp.lambdify(tmp.free_symbols, tmp)
+                lambda_list.append((foo[0].strip(), lam_1, tmp.free_symbols))
 
 print(variable_subs)
+lambda_list = sorted(lambda_list, key=lambda x: int(x[0].split("_")[1]))
 print(lambda_list)
 
 
-def runge_kutta(x, u, tau, x0=0, h=0.01):
+def runge_kutta(x, u, tau, x0=0, nint=10):
     # h is 0.01, may be adjusted later, x0 is 0
-    n = int((tau - x0) / h)
+    # n = int((tau - x0) / h)
+    h = tau/nint
+    # everywhere fix h as 10
+    # tau = 1 for 10 rooms
+
     # Step height h, step size n
     k0 = [None] * len(x)
     k1 = [None] * len(x)
     k2 = [None] * len(x)
     k3 = [None] * len(x)
-    for iter in range(1, n + 1):
+    for iter in range(1, nint + 1):
         # "Apply Runge Kutta Formulas to find next value of y and z"
         for i in range(len(x)):
             k0[i] = h * computation(i, x, u, lambda_list)
@@ -53,30 +59,23 @@ def runge_kutta(x, u, tau, x0=0, h=0.01):
         for i in range(len(x)):
             k2[i] = h * computation(i, [(x[j] + 0.5 * k1[j]) for j in range(len(x))], u, lambda_list)
         for i in range(len(x)):
-            k3[i] = h * computation(i, [(x[j] + 0.5 * k2[j]) for j in range(len(x))], u, lambda_list)
+            k3[i] = h * computation(i, [(x[j] + k2[j]) for j in range(len(x))], u, lambda_list)
         for i in range(len(x)):
             x[i] = x[i] + (1.0 / 6.0) * (k0[i] + 2 * k1[i] + 2 * k2[i] + k3[i])
-        x0 = x0 + h
 
     return x
 
 
 def computation(index, x, u, ll):
     new_vl = []
-    for i in range(len(x)):
-        new_vl.append(('x_' + str(i), x[i]))
-    for i in range(len(u)):
-        new_vl.append(('u_' + str(i), u[i]))
-    return float(ll[index][1].subs(new_vl))
+    for name in ll[index][2]:
+        spilt_of_var = (str(name)).split('_')
+        if spilt_of_var[0] == 'x':
+            new_vl.append(x[int(spilt_of_var[1])])
+        else:
+            new_vl.append(u[int(spilt_of_var[1])])
+    return float(ll[index][1](*tuple(new_vl)))
 
 
 print(runge_kutta([3.5, 0], [-3], 0.3))
 
-
-# args = sorted(lam_1.free_symbols, key=lambda x: int(str(x).split("_")[1]))
-# lam_2 = sp.lambdify(args, lam_1)
-# print(lam_2)
-# print(lam_1)
-# print(lam_2(1, 1))
-#
-# print(lam_1.free_symbols)
