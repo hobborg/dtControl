hobborg/dtControl

commit 9787316a2e11f07b38895474eab4576b401da674

Chang inside impurity MAX_INT --> np.inf

dtcontrol/decision_tree/decision_tree.py
@@ -169,7 +169,7 @@ class Node:
         for split in splits:
             impurity = self.impurity_measure.calculate_impurity(dataset, split)
             # TODO max_int...
-            if impurity < 9223372036854775807:
+            if impurity < np.inf:
                 if split.priority == 0:
                     fallback_dict[split] = impurity
                 elif split.priority <= 1 or split.priority > 0:

dtcontrol/decision_tree/impurity/auroc.py
@@ -1,5 +1,3 @@
-import sys
-
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
@@ -9,13 +7,13 @@ from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import Dete
 class AUROC(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return sys.maxsize
+            return np.inf
         scores = []
         for mask in split.get_masks(dataset):
             subset_labels = self.determinizer.determinize(dataset.from_mask_optimized(mask))
             scores.append(self.calculate_auroc(dataset.x[mask], subset_labels))
         if any([s == 0 for s in scores]):
-            return sys.maxsize
+            return np.inf
         return 1 / sum(scores)
 
     @staticmethod

dtcontrol/decision_tree/impurity/entropy.py
@@ -1,5 +1,3 @@
-import sys
-
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import DeterminizingImpurityMeasure
@@ -7,12 +5,12 @@ from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import Dete
 class Entropy(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return sys.maxsize
+            return np.inf
         impurity = 0
         for mask in split.get_masks(dataset):
             subset_labels = self.determinizer.determinize(dataset.from_mask_optimized(mask))
             if len(subset_labels) == 0:
-                return sys.maxsize
+                return np.inf
             impurity += (len(subset_labels) / len(dataset)) * self.calculate_entropy(subset_labels)
         return impurity
 

dtcontrol/decision_tree/impurity/entropy_ratio.py
@@ -1,5 +1,3 @@
-import sys
-
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import DeterminizingImpurityMeasure
@@ -8,7 +6,7 @@ class EntropyRatio(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if any(np.all(mask == False) for mask in split.get_masks(dataset)) or \
                 len(split.get_masks(dataset)) == 1:
-            return sys.maxsize
+            return np.inf
 
         split_entropy = self.calculate_split_entropy(dataset, split)
         split_info = self.calculate_split_info(dataset, split)

dtcontrol/decision_tree/impurity/gini_index.py
@@ -1,5 +1,3 @@
-import sys
-
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import DeterminizingImpurityMeasure
@@ -7,12 +5,12 @@ from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import Dete
 class GiniIndex(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return sys.maxsize
+            return np.inf
         impurity = 0
         for mask in split.get_masks(dataset):
             subset_labels = self.determinizer.determinize(dataset.from_mask_optimized(mask))
             if len(subset_labels) == 0:
-                return sys.maxsize
+                return np.inf
             impurity += (len(subset_labels) / len(dataset)) * self.calculate_gini_index(subset_labels)
         return impurity
 

dtcontrol/decision_tree/impurity/max_minority.py
@@ -1,5 +1,3 @@
-import sys
-
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import DeterminizingImpurityMeasure
@@ -7,12 +5,12 @@ from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import Dete
 class MaxMinority(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return sys.maxsize
+            return np.inf
         minorities = []
         for mask in split.get_masks(dataset):
             subset_labels = self.determinizer.determinize(dataset.from_mask_optimized(mask))
             if len(subset_labels) == 0:
-                return sys.maxsize
+                return np.inf
             minorities.append(self.calculate_minority(subset_labels))
         return max(minorities)
 

dtcontrol/decision_tree/impurity/multi_label_twoing_rule.py
@@ -1,5 +1,3 @@
-import sys
-
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.multi_label_impurity_measure import MultiLabelImpurityMeasure
@@ -7,7 +5,7 @@ from dtcontrol.decision_tree.impurity.multi_label_impurity_measure import MultiL
 class MultiLabelTwoingRule(MultiLabelImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return sys.maxsize
+            return np.inf
         assert len(split.get_masks(dataset)) == 2
         y = dataset.get_single_labels()
 
@@ -19,7 +17,7 @@ class MultiLabelTwoingRule(MultiLabelImpurityMeasure):
         right_flat = right.flatten()
         right_flat = right_flat[right_flat != -1]
         if len(left) == 0 or len(right) == 0:
-            return sys.maxsize
+            return np.inf
         num_labels = len(y)
         twoing_value = (len(left) / num_labels) * (len(right) / num_labels)
 
@@ -45,5 +43,5 @@ class MultiLabelTwoingRule(MultiLabelImpurityMeasure):
             s += abs(num_left / len(left) - num_right / len(right))
         twoing_value *= s * s
         if twoing_value == 0:
-            return sys.maxsize
+            return np.inf
         return 1 / twoing_value

dtcontrol/decision_tree/impurity/scaled_bincount.py
@@ -1,5 +1,3 @@
-import sys
-
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.multi_label_impurity_measure import MultiLabelImpurityMeasure
@@ -10,13 +8,13 @@ class ScaledBincount(MultiLabelImpurityMeasure):
 
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return sys.maxsize
+            return np.inf
         y = dataset.get_single_labels()
         impurity = 0
         for mask in split.get_masks(dataset):
             subset = y[mask, :]
             if len(subset) == 0:
-                return sys.maxsize
+                return np.inf
             impurity += (len(subset) / len(y)) * self.calculate_scaled_bincount(subset)
         return impurity
 

commit 0c4c9254c7b2dd9607d4b5b2ac00288a372f52ae

Fix weird issue where wlan was running forever. The problem was in commit 9787316a2e11f07b38895474eab4576b401da674 which introduced np.inf instead of sys.maxsize as done until the previous commit (657f7e53314d05b20fc6327a8708ba692c7ee8e6). The problem is weird and we don't have any explanations for it.

dtcontrol/decision_tree/decision_tree.py
@@ -202,7 +202,7 @@ class Node:
 
         for split in splits:
             impurity = self.impurity_measure.calculate_impurity(dataset, split)
-            if impurity < np.inf:
+            if impurity < 9223372036854775807:
                 if split.priority == 0:
                     fallback_dict[split] = impurity
                 elif split.priority <= 1 or split.priority > 0:

dtcontrol/decision_tree/impurity/auroc.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
@@ -7,13 +9,13 @@ from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import Dete
 class AUROC(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return np.inf
+            return sys.maxsize
         scores = []
         for mask in split.get_masks(dataset):
             subset_labels = self.determinizer.determinize(dataset.from_mask_optimized(mask))
             scores.append(self.calculate_auroc(dataset.x[mask], subset_labels))
         if any([s == 0 for s in scores]):
-            return np.inf
+            return sys.maxsize
         return 1 / sum(scores)
 
     @staticmethod

dtcontrol/decision_tree/impurity/entropy.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import DeterminizingImpurityMeasure
@@ -5,12 +7,12 @@ from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import Dete
 class Entropy(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return np.inf
+            return sys.maxsize
         impurity = 0
         for mask in split.get_masks(dataset):
             subset_labels = self.determinizer.determinize(dataset.from_mask_optimized(mask))
             if len(subset_labels) == 0:
-                return np.inf
+                return sys.maxsize
             impurity += (len(subset_labels) / len(dataset)) * self.calculate_entropy(subset_labels)
         return impurity
 

dtcontrol/decision_tree/impurity/entropy_ratio.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import DeterminizingImpurityMeasure
@@ -6,7 +8,7 @@ class EntropyRatio(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if any(np.all(mask == False) for mask in split.get_masks(dataset)) or \
                 len(split.get_masks(dataset)) == 1:
-            return np.inf
+            return sys.maxsize
 
         split_entropy = self.calculate_split_entropy(dataset, split)
         split_info = self.calculate_split_info(dataset, split)

dtcontrol/decision_tree/impurity/gini_index.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import DeterminizingImpurityMeasure
@@ -5,12 +7,12 @@ from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import Dete
 class GiniIndex(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return np.inf
+            return sys.maxsize
         impurity = 0
         for mask in split.get_masks(dataset):
             subset_labels = self.determinizer.determinize(dataset.from_mask_optimized(mask))
             if len(subset_labels) == 0:
-                return np.inf
+                return sys.maxsize
             impurity += (len(subset_labels) / len(dataset)) * self.calculate_gini_index(subset_labels)
         return impurity
 

dtcontrol/decision_tree/impurity/max_minority.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import DeterminizingImpurityMeasure
@@ -5,12 +7,12 @@ from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import Dete
 class MaxMinority(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return np.inf
+            return sys.maxsize
         minorities = []
         for mask in split.get_masks(dataset):
             subset_labels = self.determinizer.determinize(dataset.from_mask_optimized(mask))
             if len(subset_labels) == 0:
-                return np.inf
+                return sys.maxsize
             minorities.append(self.calculate_minority(subset_labels))
         return max(minorities)
 

dtcontrol/decision_tree/impurity/multi_label_twoing_rule.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.multi_label_impurity_measure import MultiLabelImpurityMeasure
@@ -5,7 +7,7 @@ from dtcontrol.decision_tree.impurity.multi_label_impurity_measure import MultiL
 class MultiLabelTwoingRule(MultiLabelImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return np.inf
+            return sys.maxsize
         assert len(split.get_masks(dataset)) == 2
         y = dataset.get_single_labels()
 
@@ -17,7 +19,7 @@ class MultiLabelTwoingRule(MultiLabelImpurityMeasure):
         right_flat = right.flatten()
         right_flat = right_flat[right_flat != -1]
         if len(left) == 0 or len(right) == 0:
-            return np.inf
+            return sys.maxsize
         num_labels = len(y)
         twoing_value = (len(left) / num_labels) * (len(right) / num_labels)
 
@@ -43,5 +45,5 @@ class MultiLabelTwoingRule(MultiLabelImpurityMeasure):
             s += abs(num_left / len(left) - num_right / len(right))
         twoing_value *= s * s
         if twoing_value == 0:
-            return np.inf
+            return sys.maxsize
         return 1 / twoing_value

dtcontrol/decision_tree/impurity/scaled_bincount.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.multi_label_impurity_measure import MultiLabelImpurityMeasure
@@ -8,13 +10,13 @@ class ScaledBincount(MultiLabelImpurityMeasure):
 
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return np.inf
+            return sys.maxsize
         y = dataset.get_single_labels()
         impurity = 0
         for mask in split.get_masks(dataset):
             subset = y[mask, :]
             if len(subset) == 0:
-                return np.inf
+                return sys.maxsize
             impurity += (len(subset) / len(y)) * self.calculate_scaled_bincount(subset)
         return impurity
 

dtcontrol/decision_tree/impurity/sum_minority.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import DeterminizingImpurityMeasure
@@ -5,12 +7,12 @@ from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import Dete
 class SumMinority(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return np.inf
+            return sys.maxsize
         minorities = []
         for mask in split.get_masks(dataset):
             subset_labels = self.determinizer.determinize(dataset.from_mask_optimized(mask))
             if len(subset_labels) == 0:
-                return np.inf
+                return sys.maxsize
             minorities.append(self.calculate_minority(subset_labels))
         return sum(minorities)
 

dtcontrol/decision_tree/impurity/twoing_rule.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import DeterminizingImpurityMeasure
@@ -5,13 +7,13 @@ from dtcontrol.decision_tree.impurity.determinizing_impurity_measure import Dete
 class TwoingRule(DeterminizingImpurityMeasure):
     def calculate_impurity(self, dataset, split):
         if len(split.get_masks(dataset)) == 1:
-            return np.inf
+            return sys.maxsize
         assert len(split.get_masks(dataset)) == 2
         [left_mask, right_mask] = split.get_masks(dataset)
         left = self.determinizer.determinize(dataset.from_mask_optimized(left_mask))
         right = self.determinizer.determinize(dataset.from_mask_optimized(right_mask))
         if len(left) == 0 or len(right) == 0:
-            return np.inf
+            return sys.maxsize
         num_labels = len(dataset)
         twoing_value = (len(left) / num_labels) * (len(right) / num_labels)
         s = 0
@@ -22,7 +24,7 @@ class TwoingRule(DeterminizingImpurityMeasure):
             s += abs(num_left / len(left) - num_right / len(right))
         twoing_value *= s * s
         if twoing_value == 0:
-            return np.inf
+            return sys.maxsize
         return 1 / twoing_value
 
     def get_oc1_name(self):